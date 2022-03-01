034657829c6319aff7fa0bb1591a4644a38f1f48
FERHAOUI YANISSE p1909519 <yanisse.ferhaoui@etu.univ-lyon1.fr>
2022-03-01 08:31:44 +0100

    correction

--- src/doc/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['image_2ecpp_28',['Image.cpp',['../Image_8cpp.html',1,'']]],
-  ['image_2eh_29',['Image.h',['../Image_8h.html',1,'']]]
-];
--- src/doc/html/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7eimage_53',['~Image',['../classImage.html#a0294f63700543e11c0f0da85601c7ae5',1,'Image']]]
-];
